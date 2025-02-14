4416410bee166db1f9aa8393995388966916f22d
MiguelDaehn <migueldaehnribeiro@gmail.com>
2025-02-14 14:36:39 -0300

    Trying to fix the thrust coefficient.

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ from motor import *
 #  Ok apparently it surges when the expansion ratio goes above 9 or 10
 #  9:
 #  YOU NEED TO FIX THE CF (THRUST COEFFICIENT) SOONER RATHER THAN LATER THIS IS A SERIOUS ISSUE
+#  10: add an eng(motor, path) that creates a .eng file for the motor at the path (such as the openrocket folder for convenience)
 
 def plt_m_grains(N,id_prop,arr_props,motor,eta_noz=0.85,AeAt=6.3):
     for prop in arr_props:
